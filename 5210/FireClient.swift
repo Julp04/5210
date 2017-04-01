12300a8e9b65cfacbab77e11e2b5b96959f48b93
dks0398255 <julpanucci@gmail.com>
2017-03-31 23:12:33 -0400

    added FireClient, and script to support Sketch

---
+++ 5210/FireClient.swift
@@ -0,0 +1,24 @@
+//
+//  FireClient.swift
+//  5210
+//
+//  Created by Panucci, Julian R on 3/31/17.
+//  Copyright © 2017 Panucci, Julian R. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+
+//Class to handle all Firebase calls and querys from database
+class FireClient {
+    
+    let ref = FIRDatabase.database().reference()
+    let user = FIRAuth.auth()!.currentUser!
+
+    
+    
+    
+    
+}
--- 5210/SignupViewController.swift
+++ 5210/SignupViewController.swift
@@ -13,8 +13,7 @@ class SignupViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-
 
 }
